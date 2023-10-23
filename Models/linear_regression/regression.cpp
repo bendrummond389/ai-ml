013e7f0983c9a86bcc31e6cc4c87de27b6f5bc00
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-23 17:25:25 -0600

    added in display functions and laid ground work for regression

--- Models/linear_regression/regression.cpp
+++ Models/linear_regression/regression.cpp
@@ -14,15 +14,21 @@ public:
   double computeCost();
   void gradientDescent(double alpha, int iterations);
   double predict(const Eigen::VectorXd &input);
+  void displayX();
+  void displayY();
+  void displayTheta();
 
 private:
-  Eigen::VectorXd x;
+  Eigen::MatrixXd x;
   Eigen::VectorXd y;
   Eigen::VectorXd theta;
   int m;
 };
 
-LinearRegression::LinearRegression() { m = 0; }
+LinearRegression::LinearRegression() {
+  m = 0;
+  theta = Eigen::VectorXd::Zero(2);
+}
 
 LinearRegression::~LinearRegression() {
   // Cleanup code
@@ -30,7 +36,10 @@ LinearRegression::~LinearRegression() {
 
 void LinearRegression::input(const Eigen::VectorXd &x_values,
                              const Eigen::VectorXd &y_values) {
-  x = x_values;
+  m = x_values.size();
+  x = Eigen::MatrixXd(m, 2);           // Matrix with m rows and 2 columns
+  x.col(0) = Eigen::VectorXd::Ones(m); // First column as ones
+  x.col(1) = x_values;
   y = y_values;
 }
 
@@ -48,4 +57,47 @@ double LinearRegression::predict(const Eigen::VectorXd &input) {
   return 0.0;
 }
 
-int main(void) { std::cout << "hello" << std::endl; }
+void LinearRegression::displayX() {
+  std::cout << "X matrix (input features): \n";
+  std::cout << x << std::endl;
+}
+
+void LinearRegression::displayY() {
+  std::cout << "y vector (output values): \n";
+  std::cout << y << std::endl;
+}
+
+void LinearRegression::displayTheta() {
+  std::cout << "Theta vector (model parameters): \n";
+  std::cout << theta << std::endl;
+}
+
+int main() {
+  // Sample dataset
+  Eigen::VectorXd x_values(5);
+  x_values << 1.0, 2.0, 3.0, 4.0, 5.0; // Input feature values
+
+  Eigen::VectorXd y_values(5);
+  y_values << 2.0, 4.0, 5.5, 8.0, 10.0; // Corresponding output values
+
+  // Create a LinearRegression object
+  LinearRegression lr;
+
+  // Input the sample data
+  lr.input(x_values, y_values);
+
+  // Display the data structures
+  std::cout << "Displaying X matrix:" << std::endl;
+  lr.displayX();
+  std::cout << "\n------------------------\n";
+
+  std::cout << "Displaying y vector:" << std::endl;
+  lr.displayY();
+  std::cout << "\n------------------------\n";
+
+  std::cout << "Displaying theta vector:" << std::endl;
+  lr.displayTheta();
+  std::cout << "\n------------------------\n";
+
+  return 0;
+}
