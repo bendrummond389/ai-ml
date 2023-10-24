3bc5dc7a420916d3bb71753ae65c9dd4b41a709f
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-23 18:20:37 -0600

    gradient descent working

--- Models/linear_regression/regression.cpp
+++ Models/linear_regression/regression.cpp
@@ -12,7 +12,8 @@ public:
 
   void input(const Eigen::VectorXd &x_values, const Eigen::VectorXd &y_values);
   double computeCost();
-  void gradientDescent(double alpha, int iterations);
+  void gradientDescent(double alpha, int iterations,
+                       double convergence_threshold);
   double predict(const Eigen::VectorXd &input);
   void displayX();
   void displayY();
@@ -22,12 +23,14 @@ private:
   Eigen::MatrixXd x;
   Eigen::VectorXd y;
   Eigen::VectorXd theta;
+  double prev_cost;
   int m;
 };
 
 LinearRegression::LinearRegression() {
   m = 0;
   theta = Eigen::VectorXd::Zero(2);
+  prev_cost = 0.0;
 }
 
 LinearRegression::~LinearRegression() {
@@ -37,19 +40,38 @@ LinearRegression::~LinearRegression() {
 void LinearRegression::input(const Eigen::VectorXd &x_values,
                              const Eigen::VectorXd &y_values) {
   m = x_values.size();
-  x = Eigen::MatrixXd(m, 2);           // Matrix with m rows and 2 columns
-  x.col(0) = Eigen::VectorXd::Ones(m); // First column as ones
+  x = Eigen::MatrixXd(m, 2);
+  x.col(0) = Eigen::VectorXd::Ones(m);
   x.col(1) = x_values;
   y = y_values;
 }
 
 double LinearRegression::computeCost() {
-  // Placeholder for cost computation
-  return 0.0;
+  Eigen::VectorXd h = x * theta;
+  double cost = 0.0;
+  for (int i = 0; i < m; i++) {
+    double error = h[i] - y[i];
+    cost += error * error;
+  }
+  return cost / (2 * m);
 }
 
-void LinearRegression::gradientDescent(double alpha, int iterations) {
-  // Placeholder for gradient descent algorithm
+void LinearRegression::gradientDescent(double alpha, int iterations,
+                                       double convergence_threshold) {
+  for (int iter = 0; iter < iterations; iter++) {
+    Eigen::VectorXd h = x * theta;
+    Eigen::VectorXd error = h - y;
+
+    theta(0) -= alpha * (error.sum() / m);
+    theta(1) -= alpha * (x.col(1).cwiseProduct(error)).sum() / m;
+
+    double cost = computeCost();
+
+    if (iter > 0 && abs(prev_cost - cost) < convergence_threshold) {
+      break;
+    }
+    prev_cost = cost;
+  }
 }
 
 double LinearRegression::predict(const Eigen::VectorXd &input) {
@@ -73,12 +95,13 @@ void LinearRegression::displayTheta() {
 }
 
 int main() {
-  // Sample dataset
-  Eigen::VectorXd x_values(5);
-  x_values << 1.0, 2.0, 3.0, 4.0, 5.0; // Input feature values
+  // Extended sample dataset with more variance
+  Eigen::VectorXd x_values(10);
+  x_values << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0; // Input feature values
 
-  Eigen::VectorXd y_values(5);
-  y_values << 2.0, 4.0, 5.5, 8.0, 10.0; // Corresponding output values
+  Eigen::VectorXd y_values(10);
+  y_values << 2.3, 4.7, 6.2, 7.8, 9.5, 11.1, 12.9, 14.4, 16.6, 18.7; // Corresponding output values with variance
+
 
   // Create a LinearRegression object
   LinearRegression lr;
@@ -86,16 +109,11 @@ int main() {
   // Input the sample data
   lr.input(x_values, y_values);
 
+  // gradient descent
+  lr.gradientDescent(0.001, 1000, 0.001);
+
   // Display the data structures
-  std::cout << "Displaying X matrix:" << std::endl;
-  lr.displayX();
-  std::cout << "\n------------------------\n";
-
-  std::cout << "Displaying y vector:" << std::endl;
-  lr.displayY();
-  std::cout << "\n------------------------\n";
-
-  std::cout << "Displaying theta vector:" << std::endl;
+  std::cout << "Displaying theta after gradient descent" << std::endl;
   lr.displayTheta();
   std::cout << "\n------------------------\n";
 
