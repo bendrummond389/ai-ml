b5e1039c596d8e4c8a3a046e28d4284904eaa7dc
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-23 18:49:11 -0600

    Cleaned and added comments
    Used chatGPT to clean and add comments

--- Models/linear_regression/regression.cpp
+++ Models/linear_regression/regression.cpp
@@ -1,41 +1,44 @@
 #include <Eigen/Dense>
 #include <cmath>
 #include <iostream>
-#include <map>
-#include <sstream>
-#include <vector>
 
+// Linear Regression class for single variable regression
 class LinearRegression {
 public:
-  LinearRegression();
-  ~LinearRegression();
+  LinearRegression();  // Constructor
+  ~LinearRegression(); // Destructor
 
+  // Method to input training data
   void input(const Eigen::VectorXd &x_values, const Eigen::VectorXd &y_values);
+
+  // Method to compute the cost
   double computeCost();
+
+  // Method to perform gradient descent
   void gradientDescent(double alpha, int iterations,
                        double convergence_threshold);
+
+  // Method to predict a new input value
   double predict(const Eigen::VectorXd &input);
+
+  // Methods to display internal data structures
   void displayX();
   void displayY();
   void displayTheta();
 
 private:
-  Eigen::MatrixXd x;
-  Eigen::VectorXd y;
-  Eigen::VectorXd theta;
-  double prev_cost;
-  int m;
+  Eigen::MatrixXd x;     // Matrix of input features
+  Eigen::VectorXd y;     // Vector of output values
+  Eigen::VectorXd theta; // Vector of model parameters
+  double prev_cost;      // Store the previous cost for convergence check
+  int m;                 // Number of training examples
 };
 
-LinearRegression::LinearRegression() {
-  m = 0;
-  theta = Eigen::VectorXd::Zero(2);
-  prev_cost = 0.0;
+LinearRegression::LinearRegression() : m(0), prev_cost(0.0) {
+  theta = Eigen::VectorXd::Zero(2); // Initialize theta with zeros
 }
 
-LinearRegression::~LinearRegression() {
-  // Cleanup code
-}
+LinearRegression::~LinearRegression() {}
 
 void LinearRegression::input(const Eigen::VectorXd &x_values,
                              const Eigen::VectorXd &y_values) {
@@ -48,12 +51,8 @@ void LinearRegression::input(const Eigen::VectorXd &x_values,
 
 double LinearRegression::computeCost() {
   Eigen::VectorXd h = x * theta;
-  double cost = 0.0;
-  for (int i = 0; i < m; i++) {
-    double error = h[i] - y[i];
-    cost += error * error;
-  }
-  return cost / (2 * m);
+  Eigen::VectorXd errors = h - y;
+  return errors.squaredNorm() / (2 * m);
 }
 
 void LinearRegression::gradientDescent(double alpha, int iterations,
@@ -62,12 +61,11 @@ void LinearRegression::gradientDescent(double alpha, int iterations,
     Eigen::VectorXd h = x * theta;
     Eigen::VectorXd error = h - y;
 
-    theta(0) -= alpha * (error.sum() / m);
-    theta(1) -= alpha * (x.col(1).cwiseProduct(error)).sum() / m;
+    theta -= alpha * (x.transpose() * error) / m;
 
     double cost = computeCost();
 
-    if (iter > 0 && abs(prev_cost - cost) < convergence_threshold) {
+    if (iter > 0 && std::abs(prev_cost - cost) < convergence_threshold) {
       break;
     }
     prev_cost = cost;
@@ -75,45 +73,34 @@ void LinearRegression::gradientDescent(double alpha, int iterations,
 }
 
 double LinearRegression::predict(const Eigen::VectorXd &input) {
-  // Placeholder for prediction function
-  return 0.0;
+  return 0.0; // Placeholder for prediction function
 }
 
 void LinearRegression::displayX() {
-  std::cout << "X matrix (input features): \n";
-  std::cout << x << std::endl;
+  std::cout << "X matrix (input features): \n" << x << std::endl;
 }
 
 void LinearRegression::displayY() {
-  std::cout << "y vector (output values): \n";
-  std::cout << y << std::endl;
+  std::cout << "y vector (output values): \n" << y << std::endl;
 }
 
 void LinearRegression::displayTheta() {
-  std::cout << "Theta vector (model parameters): \n";
-  std::cout << theta << std::endl;
+  std::cout << "Theta vector (model parameters): \n" << theta << std::endl;
 }
 
 int main() {
-  // Extended sample dataset with more variance
+  // Sample dataset
   Eigen::VectorXd x_values(10);
-  x_values << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0; // Input feature values
+  x_values << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0;
 
   Eigen::VectorXd y_values(10);
-  y_values << 2.3, 4.7, 6.2, 7.8, 9.5, 11.1, 12.9, 14.4, 16.6, 18.7; // Corresponding output values with variance
+  y_values << 2.3, 4.7, 6.2, 7.8, 9.5, 11.1, 12.9, 14.4, 16.6, 18.7;
 
-
-  // Create a LinearRegression object
   LinearRegression lr;
-
-  // Input the sample data
   lr.input(x_values, y_values);
-
-  // gradient descent
   lr.gradientDescent(0.001, 1000, 0.001);
 
-  // Display the data structures
-  std::cout << "Displaying theta after gradient descent" << std::endl;
+  std::cout << "Displaying theta after gradient descent:" << std::endl;
   lr.displayTheta();
   std::cout << "\n------------------------\n";
 
